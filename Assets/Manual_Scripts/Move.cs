ba8a54f47ffee8f2cfd3b507f684191bb1e314ee
Michael Little <little_michael101@aol.com>
2024-01-19 17:01:08 -0500

    Phase 1 complete

--- Assets/Manual_Scripts/Move.cs
+++ Assets/Manual_Scripts/Move.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -12,41 +14,89 @@ public class NewBehaviourScript : MonoBehaviour
     private GameObject _attachTransform;
     [SerializeField]
     private GameObject _2ndPipe;
+    [SerializeField]
+    private GameObject _thinnerCan1;
+    [SerializeField]
+    private GameObject _thinnerCan2;
+    [SerializeField]
+    private GameObject _gun;
     private float _speed = .003f;
-    private bool _didBoxMove = true;
-
+    private bool _isSocket1 = false;
+    private bool _isSocket2 = false;
+    private bool _movingToward = false;
+    public enum MoveState
+    {
+        Stay,
+        Move,
+        Stop
+    }
+    public MoveState currentState;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentState = MoveState.Stay;
     }
-
     // Update is called once per frame
     void Update()
     {
-        BurnBoxMove();
+        switch (currentState)
+        {
+            case MoveState.Stay:
+                _speed = 0;
+                break;
+            case MoveState.Move:
+                _movingToward = true;
+                _speed = .003f;
+                StartCoroutine(BurnBoxMove());
+                break;
+            case MoveState.Stop:
+                StopAllCoroutines();
+                break;
+        }
+        if (transform.position != _boxEndPoint.transform.position && _movingToward == true)
+        {
+            currentState = MoveState.Move;
+        }     
     }
 
-    private void BurnBoxMove()
+    IEnumerator BurnBoxMove()
     {
-        if (transform.position != _boxEndPoint.transform.position && _didBoxMove == false)
+        yield return new WaitForSeconds(.001f);
+        if (transform.position != _boxEndPoint.transform.position)
         {
             this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position,
                 _boxEndPoint.transform.position, _speed);
         }
-        else if (transform.position == _boxEndPoint.transform.position)
+        else if (transform.position == _boxEndPoint.transform.position && _thinnerCan2.gameObject != null && _thinnerCan1.gameObject != null)
+        {  
+            _thinnerCan1.gameObject.SetActive(true);
+            _thinnerCan2.gameObject.SetActive(true);
+            currentState = MoveState.Stop;    
+            Debug.Log("Did the method truly stop?");
+        }
+    }
+    public void Socket1()
+    {
+        _isSocket1 = true;
+    }
+    public void Socket2()
+    {
+        _isSocket2 = true;
+    }
+    public void BothSockets()
+    {
+        if(_isSocket1 == true && _isSocket2 == true)
         {
-            _didBoxMove = true;
-            Debug.Log("Did the method stop?");
+            _gun.gameObject.SetActive(true);
         }
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "NudgePoint")
         {
+            currentState = MoveState.Move;
             Destroy(_disappearingBoxCollider.gameObject);
             _attachTransform.gameObject.SetActive(true);
-            _didBoxMove = false;
             Destroy(_2ndPipe.gameObject, 1.5f);
         }
     }
--- Assets/Manual_Scripts/Switch.cs
+++ Assets/Manual_Scripts/Switch.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.CompilerServices;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Switch : MonoBehaviour
@@ -20,8 +21,43 @@ public class Switch : MonoBehaviour
     private GameObject _spark;
     [SerializeField]
     private GameObject _chains;
+    [SerializeField]
+    private GameObject _gunSpark;
+    [SerializeField]
+    private GameObject _explosion1;
+    [SerializeField] 
+    private GameObject _explosion2;
+    [SerializeField] 
+    private GameObject _explosion3;
+    [SerializeField] 
+    private GameObject _boxFlame;
+    [SerializeField]
+    private GameObject _cageFlame;
+    [SerializeField]
+    private GameObject _cagebox;
+    [SerializeField]
+    private GameObject _burnBox;
+    [SerializeField]
+    private GameObject _thinnerCan1;
+    [SerializeField]
+    private GameObject _thinnerCan2;
+    [SerializeField]
+    private GameObject _oilCan;
+    [SerializeField]
+    private GameObject _smoke1;
+    [SerializeField]
+    private GameObject _smoke2;
+    [SerializeField]
+    private GameObject _explosionPointLight1;
+    [SerializeField]
+    private GameObject _explosionPointLight2;
+    [SerializeField] 
+    private GameObject _explosionPointLight3;
+    [SerializeField] 
+    private GameObject _igniteSpark;
     private bool _isKey1 = false;
     private bool _isKey2 = false;
+    private bool _isGunHovering = false;
 
     // Start is called before the first frame update
     void Start()
@@ -66,6 +102,51 @@ public class Switch : MonoBehaviour
             Destroy(_topLock.gameObject, 5);
         }
     }
+    //Next 3 methods cover gunshot.
+    public void GunCheck()
+    {
+        _isGunHovering = true;
+    }
+    public void GunExits()
+    {
+        _isGunHovering = false;
+    }
+    public void GunHover()
+    {
+        if(_isGunHovering == true)
+        {
+            _gunSpark.gameObject.SetActive(true);
+            StartCoroutine(UpInFlames());
+        }
+    }
+
+    IEnumerator UpInFlames()
+    {
+        yield return new WaitForSeconds(.06f);
+        _igniteSpark.gameObject.SetActive(true);
+        _explosion1.gameObject.SetActive(true);
+        _explosion2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.7f);
+        _explosionPointLight1.gameObject.SetActive(true);
+        _explosionPointLight2.gameObject.SetActive(true);
+        _burnBox.gameObject.SetActive(false);
+        Destroy(_thinnerCan1.gameObject);
+        Destroy(_thinnerCan2.gameObject);
+        _boxFlame.gameObject.SetActive(true);
+        _smoke1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.3f);       
+        yield return new WaitForSeconds(.2f);
+        _cageFlame.gameObject.SetActive(true);
+        _smoke2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.2f);
+        _explosion3.gameObject.SetActive(true);
+        yield return new WaitForSeconds(.5f);
+        Destroy(_oilCan.gameObject);
+        yield return new WaitForSeconds(.2f);
+        _cagebox.gameObject.SetActive(false);
+        _explosionPointLight3.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1);        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -75,5 +156,4 @@ public class Switch : MonoBehaviour
             Destroy(_chains.gameObject);
         }
     }
-
 }
